f3121a4ad00137058e4fa1bfac975a0d532abf3b
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-02-06 16:59:29 -0500

    Create modal, overlay, and form

--- books.js
+++ books.js
@@ -1,3 +1,11 @@
+//Grab DOM elements.//
+
+const addBtn = document.getElementsByClassName("add");
+const closeModalBtn = document.getElementsByClassName("close");
+const overlay = document.getElementsByClassName("overlay");
+
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,6 +15,8 @@ function Book(title, author, pages, read) {
     return this.title + this.author + this.pages + this.read;
   };
 }
+
+function addBookToLibrary() {}
 
 const book1 = new Book("title one", "Marisa Min", 100, true);
 
